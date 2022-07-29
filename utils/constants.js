08314370829772ff3cbc0880577201fd86d007fa
liyu <43615920+Ytiona@users.noreply.github.com>
2022-07-29 23:15:14 +0800

    Update constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -3,6 +3,6 @@ module.exports = {
   PRIVATE_KEY: 'BXWTIP8S1F0',//token加密密钥
   EXPIRESD: 60*60*24*10,//token有效期
   APP_ID: 'wxc1481f55417579cd',
-  APP_SECRET: '16247482aef056394c4222422ef03d15',
+  APP_SECRET: '*****',
   PIC_EXTS: '.bmp,.jpg,.png,.tif,.gif,.pcx,.tga,.exif,.fpx,.svg,.psd,.cdr,.pcd,.dxf,.ufo,.eps,.ai,.raw,.WMF,.webp'.split(',')
 }
